14a886fd96834d3166597cd00e395da161cc34af
asma <asma.kethiri@gmail.com>
2018-09-15 19:00:35 +0100

    testing

--- app/src/main/java/com/kthiri/foursimplerulesforhappierlife/MainActivity.kt
+++ app/src/main/java/com/kthiri/foursimplerulesforhappierlife/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.kthiri.foursimplerulesforhappierlife
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.kthiri.foursimplerulesforhappierlife.R.id.image_smile
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image_smile.setOnClickListener {
+            var intent  = Intent(this,SmileActivity::class.java)
+            startActivity(intent)
+
+
+        }
+
+        image_breath.setOnClickListener {
+            var intent  = Intent(this,BreathActivity::class.java)
+            startActivity(intent)
+        }
+        image_squat.setOnClickListener {
+            var intent  = Intent(this,SquatActivity::class.java)
+            startActivity(intent)
+        }
+        image_water.setOnClickListener {
+            var intent  = Intent(this,WaterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
