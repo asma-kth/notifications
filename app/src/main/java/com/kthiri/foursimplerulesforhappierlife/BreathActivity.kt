326a7488f03174e545c5c7fcc673fc339b41ebd3
asma <asma.kethiri@gmail.com>
2018-09-15 18:21:06 +0100

    notifications

---
+++ app/src/main/java/com/kthiri/foursimplerulesforhappierlife/BreathActivity.kt
@@ -0,0 +1,12 @@
+package com.kthiri.foursimplerulesforhappierlife
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BreathActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.breath)
+    }
+}
---
+++ app/src/main/java/com/kthiri/foursimplerulesforhappierlife/SmileActivity.kt
@@ -0,0 +1,12 @@
+package com.kthiri.foursimplerulesforhappierlife
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SmileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.smile)
+    }
+}
